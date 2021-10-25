84a8adad48710aa49c61e4e9e1b9c67b15476d30
Subhamverma2407 <subhamverma2407@gmail.com>
2021-10-26 01:27:09 +0530

    UI changes

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -37,7 +37,7 @@ const Transaction = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (
-      amount !== "" &&
+      amount.trim() !== "" &&
       parseInt(amount) !== 0 &&
       !isNaN(amount) &&
       transactionType !== null &&
